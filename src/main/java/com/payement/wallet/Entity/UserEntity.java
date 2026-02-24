da877fb774ac253d6120310bf4bb49bceb6f91f0
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-02-24 16:59:05 +0100

    user table

---
+++ src/main/java/com/payement/wallet/Entity/UserEntity.java
@@ -0,0 +1,41 @@
+package com.payement.wallet.Entity;
+
+import com.payement.wallet.Enum.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    private String otherNames;
+    @NotBlank
+    private  String username;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+    @NotEmpty
+    private String phoneNumber;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    private LocalDateTime createdAt;
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
+
+}
