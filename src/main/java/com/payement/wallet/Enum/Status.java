76cb529927f0200e222fdcc4ba228f70778d6da8
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-02-24 17:00:40 +0100

    account  status

---
+++ src/main/java/com/payement/wallet/Enum/Status.java
@@ -0,0 +1,20 @@
+package com.payement.wallet.Enum;
+
+public enum Status {
+    ACTIVE,
+    SUSPENDED,
+    DELETED,
+    COMPLETED,
+    FAILED,
+    PENDING,
+    SUCCESSFUL,
+    SENT,
+    VERIFIED,
+    UNVERIFIED,
+    APPROVED,
+    REJECTED,
+    RECEIVED,
+    DECLINED,
+
+
+}
