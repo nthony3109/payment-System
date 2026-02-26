d3125dbfb84edbdc780ecca43e5768a36adb107d
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-02-26 17:30:36 +0100

    added fields

--- src/main/java/com/payement/wallet/Entity/Account.java
+++ src/main/java/com/payement/wallet/Entity/Account.java
@@ -1,4 +1,30 @@
 package com.payement.wallet.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal balance;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private UserEntity user_d;
+    @Version
+    private String currency;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Long version;
 }
