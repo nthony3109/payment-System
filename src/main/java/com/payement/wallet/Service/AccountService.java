749451ef1ca2aed993027091f894c9cc82d632d5
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-03-05 23:22:44 +0100

    updated

--- src/main/java/com/payement/wallet/Service/AccountService.java
+++ src/main/java/com/payement/wallet/Service/AccountService.java
@@ -1,11 +1,17 @@
 package com.payement.wallet.Service;
 
+import com.payement.wallet.Entity.Account;
+import com.payement.wallet.Exceptions.AccountNotFoundException;
+import com.payement.wallet.Repo.AccountRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
+    private final AccountRepo accountRepo;
 
     public String createAccountNumber(String phoneNumber) {
         String accountNumber;
@@ -19,5 +25,17 @@ public class AccountService {
 
         }
         return accountNumber;
+    }
+
+    public boolean deposit (BigDecimal amount, String accountNumber) {
+
+        Account account = accountRepo.findByAccountNumber(accountNumber);
+        if (account == null) {
+            throw new AccountNotFoundException("account number is not found");
+        }
+        account.setBalance(account.getBalance().add(amount));
+        accountRepo.save(account);
+        return true;
+
     }
 }
