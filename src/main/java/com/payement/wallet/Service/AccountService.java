0bdffddc826faf2504e4f28f714f0ed788a9baf6
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-03-11 17:13:18 +0100

    added methods

--- src/main/java/com/payement/wallet/Service/AccountService.java
+++ src/main/java/com/payement/wallet/Service/AccountService.java
@@ -1,10 +1,15 @@
 package com.payement.wallet.Service;
 
+import com.payement.wallet.DTOs.TransferReq;
 import com.payement.wallet.Entity.Account;
+import com.payement.wallet.Entity.UserEntity;
+import com.payement.wallet.Enum.Currency;
 import com.payement.wallet.Exceptions.AccountNotFoundException;
 import com.payement.wallet.Exceptions.InsufficientFundException;
+import com.payement.wallet.Exceptions.InvalidDepositAmountException;
 import com.payement.wallet.Exceptions.InvalidPhoneNumberException;
 import com.payement.wallet.Repo.AccountRepo;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +19,19 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepo accountRepo;
+
+
+    public Account createAccount(UserEntity user) {
+        String phoneNumber = user.getPhoneNumber();
+        String accountNumber = generateAccountNumber(phoneNumber);
+        Account account = Account.builder()
+                .user(user)
+                .accountNumber(accountNumber)
+                .balance(BigDecimal.ZERO)
+                .currency(Currency.NGN)
+                .build();
+        return accountRepo.save(account);
+    }
 
     public String generateAccountNumber(String phoneNumber) {
         String accountNumber;
@@ -31,11 +49,29 @@ public class AccountService {
         return accountNumber;
     }
 
+    public Account getAccountByUser(UserEntity user) {
+        return accountRepo.findByUser(user)
+                .orElseThrow(
+                        () -> new AccountNotFoundException("account not found for the user")
+                );
+    }
+
+    public Account getAccountByAccountNumber(String accountNumber) {
+        Account account = accountRepo.findByAccountNumber(accountNumber);
+        if (account == null) {
+            throw new AccountNotFoundException("account number is not found");
+        }
+        return account;
+    }
+
     public boolean deposit (BigDecimal amount, String accountNumber) {
 
         Account account = accountRepo.findByAccountNumber(accountNumber);
         if (account == null) {
             throw new AccountNotFoundException("account number is not found");
+        }
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new InvalidDepositAmountException("amount must be greater than zero");
         }
         account.setBalance(account.getBalance().add(amount));
         accountRepo.save(account);
@@ -61,6 +97,26 @@ public class AccountService {
             throw new AccountNotFoundException("no account found with the account number");
         }
         return account.getBalance();
+    }
+
+    @Transactional
+    public Account transfer(TransferReq req) {
+        Account fromAccount = accountRepo.findByAccountNumber(req.getFromAccountNumber());
+        if (fromAccount == null) {
+            throw new AccountNotFoundException("sender account not found");
+        }
+        Account toAccount = accountRepo.findByAccountNumber(req.getToAccountNumber());
+        if (toAccount == null) {
+            throw new AccountNotFoundException(" receiver account not found");
+        }
+        if (fromAccount.getBalance().compareTo(req.getAmount()) < 0) {
+            throw new InsufficientFundException("insufficient fund");
+        }
+        fromAccount.setBalance(fromAccount.getBalance().subtract(req.getAmount()));
+        toAccount.setBalance(toAccount.getBalance().add(req.getAmount()));
+        accountRepo.save(fromAccount);
+        accountRepo.save(toAccount);
+        return fromAccount;
     }
 
 
