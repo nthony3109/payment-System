ef1fc4553914dc2a0909f9788499f75cdd2b4fb3
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-03-13 17:59:59 +0100

    updated

--- src/main/java/com/payement/wallet/Repo/AccountRepo.java
+++ src/main/java/com/payement/wallet/Repo/AccountRepo.java
@@ -1,8 +1,12 @@
 package com.payement.wallet.Repo;
 
 import com.payement.wallet.Entity.Account;
+import com.payement.wallet.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface AccountRepo extends JpaRepository<Account,Long> {
     Account findByAccountNumber(String accountNumber);
+    Optional<Account> findByUser(UserEntity user);
 }
