1913491a9c69e1e891516a2ff0bb2b76a6321f53
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-03-16 23:30:34 +0100

    an exception

--- src/main/java/com/payement/wallet/Exceptions/InsufficientFundException.java
+++ src/main/java/com/payement/wallet/Exceptions/InsufficientFundException.java
@@ -3,6 +3,6 @@ package com.payement.wallet.Exceptions;
 public class InsufficientFundException extends RuntimeException {
     public InsufficientFundException(String message) {
         super(message);
-        System.err.println("omo ur monney oo ");// print to the console even network no dey
+        System.err.println("omo ur monney no reach oo ");
     }
 }
