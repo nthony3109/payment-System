7690eeff6d136efc5c662d4b277a546caf354356
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-03-17 23:18:02 +0100

    an exception

--- src/main/java/com/payement/wallet/Exceptions/InvalidDepositAmountException.java
+++ src/main/java/com/payement/wallet/Exceptions/InvalidDepositAmountException.java
@@ -2,6 +2,6 @@ package com.payement.wallet.Exceptions;
 
 public class InvalidDepositAmountException extends RuntimeException{
     public InvalidDepositAmountException(String message) {
-        super(message);
+        super(message); // to access the runtime exception class
     }
 }
