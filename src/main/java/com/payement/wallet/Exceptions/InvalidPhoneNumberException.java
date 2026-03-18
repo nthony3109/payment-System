e8edc9328a38558afafe5f5fb0b7469dc54d4d4d
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-03-18 23:06:35 +0100

    an exception

---
+++ src/main/java/com/payement/wallet/Exceptions/InvalidPhoneNumberException.java
@@ -0,0 +1,7 @@
+package com.payement.wallet.Exceptions;
+
+public class InvalidPhoneNumberException extends RuntimeException{
+    public InvalidPhoneNumberException(String message) {
+        super(message);
+    }
+}
