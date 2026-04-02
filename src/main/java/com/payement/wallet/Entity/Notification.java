c1991a8352be9588d099c8b31b35125e9248a1ab
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-04-02 10:34:33 +0100

    updated

--- src/main/java/com/payement/wallet/Entity/Notification.java
+++ src/main/java/com/payement/wallet/Entity/Notification.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Builder
@@ -13,8 +16,15 @@ public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "TEXT")
     private  String message;
+    private String messageTittle;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private UserEntity user;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
