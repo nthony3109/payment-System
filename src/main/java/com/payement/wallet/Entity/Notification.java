de8fcafa58afc22289d6a521811d088593b94183
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-04-27 19:33:43 +0100

    updated entity

--- src/main/java/com/payement/wallet/Entity/Notification.java
+++ src/main/java/com/payement/wallet/Entity/Notification.java
@@ -1,9 +1,7 @@
 package com.payement.wallet.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -12,6 +10,8 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,7 +20,7 @@ public class Notification {
     @Column(columnDefinition = "TEXT")
     private  String message;
     private String messageTittle;
-
+    private boolean isViewed;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private UserEntity user;
