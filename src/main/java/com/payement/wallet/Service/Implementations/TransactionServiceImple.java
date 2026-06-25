56ab1b8b4941e7546f545fb3c891edd943a75da0
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-06-25 16:28:20 +0100

    service interface implementation

--- src/main/java/com/payement/wallet/Service/Implementations/TransactionServiceImple.java
+++ src/main/java/com/payement/wallet/Service/Implementations/TransactionServiceImple.java
@@ -1,15 +1,20 @@
-package com.payement.wallet.Service;
+package com.payement.wallet.Service.Implementations;
 
+import com.payement.wallet.DTOs.DepositRes;
+import com.payement.wallet.DTOs.TransactionHistory;
 import com.payement.wallet.DTOs.TransferReq;
+import com.payement.wallet.DTOs.TransferRes;
 import com.payement.wallet.Entity.Account;
 import com.payement.wallet.Entity.Transaction;
 import com.payement.wallet.Enum.Status;
 import com.payement.wallet.Enum.Transactiontype;
 import com.payement.wallet.Exceptions.AccountNotFoundException;
+import com.payement.wallet.Exceptions.AccountOperationException;
 import com.payement.wallet.Exceptions.InsufficientFundException;
 import com.payement.wallet.Exceptions.InvalidDepositAmountException;
 import com.payement.wallet.Repo.AccountRepo;
 import com.payement.wallet.Repo.TransactionRepo;
+import com.payement.wallet.Service.interfaces.TransactionService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,12 +30,13 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class TransactionService {
+public class TransactionServiceImple implements TransactionService {
   private  final TransactionRepo transactionRepo;
   private  final AccountRepo accountRepo;
 
     // to deposit funds
-    public Transaction deposit (BigDecimal amount, String accountNumber) {
+    @Override
+    public DepositRes deposit (BigDecimal amount, String accountNumber) {
 
         Account account = accountRepo.findByAccountNumber(accountNumber);
         if (account == null) {
@@ -46,23 +52,34 @@ public class TransactionService {
     }
 
     // to transfer funds
+    @Override
     @Transactional
-    public Transaction transfer(TransferReq req) {
+    public TransferRes transfer(TransferReq req) {
+        // check for idempotency
+        Transaction tnx = transactionRepo.findByTransactionRef(req.getTransactionRef());
+        if (tnx != null) {
+            return new TransferRes(tnx);
+        }
         Account fromAccount = accountRepo.findByAccountNumber(req.getFromAccountNumber());
         if (fromAccount == null) {
             throw new AccountNotFoundException("sender account not found");
         }
+        //to check if sender account is active
+        if(fromAccount.getStatus() != Status.ACTIVE) {
+            throw new AccountOperationException("sender account is not active or frozen");
+        }
         Account toAccount = accountRepo.findByAccountNumber(req.getToAccountNumber());
-        if (toAccount == null) {
+        if (toAccount == null)
             throw new AccountNotFoundException(" receiver account not found");
-        }
-        if (fromAccount.getBalance().compareTo(req.getAmount()) < 0) {
+
+
+        if (fromAccount.getBalance().compareTo(req.getAmount()) < 0)
             throw new InsufficientFundException("insufficient fund");
-        }
-        if (req.getAmount().compareTo(BigDecimal.ZERO) <= 11) {
-            //create invalid transfer amounnt exception and throw it  here
-                throw new InvalidDepositAmountException("transfer amount must be greater than zero");
-        }
+
+
+        //to check withdrawable value
+        if (req.getAmount().compareTo(BigDecimal.ZERO) <= 0)
+            throw new InvalidDepositAmountException("transfer amount must be greater than zero");
 
         fromAccount.setBalance(fromAccount.getBalance().subtract(req.getAmount()));
         toAccount.setBalance(toAccount.getBalance().add(req.getAmount()));
@@ -71,7 +88,8 @@ public class TransactionService {
         return logTransferTransaction(fromAccount, toAccount, req.getAmount(),req.getDescription());
             }
 
-    public Transaction logDepositTransaction(Account toaccount, BigDecimal amount) {
+   @Override
+    public DepositRes logDepositTransaction(Account toaccount, BigDecimal amount) {
         log.info("inside logDepositTransaction method to create transaction");
         String transactionRef = "TXN-" + UUID.randomUUID().toString().substring(0, 8).toLowerCase();
         log.info("buiding transaction object with account {}  and amount of {}",toaccount,amount);
@@ -86,10 +104,14 @@ public class TransactionService {
                 .transactionStatus(Status.SUCCESSFUL)
                 .completedAt(LocalDateTime.now())
                 .build();
-        log.info("transaction object built: {}", transaction);
-        return transactionRepo.save(transaction);
+        transactionRepo.save(transaction);
+        log.info("transaction object built and saved: {}", transaction);
+
+        return new DepositRes(transaction);
     }
-    public Transaction logTransferTransaction(Account fromAccount, Account toAccount, BigDecimal amount, String description) {
+
+    @Override
+    public TransferRes logTransferTransaction(Account fromAccount, Account toAccount, BigDecimal amount, String description) {
         String transactionRef = "TNX-" + UUID.randomUUID().toString().substring(0,8).toLowerCase();
         Transaction transaction = Transaction.builder()
                 .fromAccount(fromAccount)
@@ -102,17 +124,23 @@ public class TransactionService {
                 .transactionStatus(Status.SUCCESSFUL)
                 .completedAt(LocalDateTime.now())
                 .build();
-        return transactionRepo.save(transaction);
+        transactionRepo.save(transaction);
+        log.info("the transfer log transaction object is built and saved: {}", transaction);
+        return new TransferRes(transaction);
     }
 
-    public List<Transaction> getTransactionHistory(Account account) {
+    @Override
+    public List<TransactionHistory> getTransactionHistory(Account account) {
         List<Transaction> sentTransactions = transactionRepo.findByFromAccount(account);
         List<Transaction> receivedTransactions = transactionRepo.findByToAccount(account);
         List<Transaction> allTransactions = new ArrayList<>();
         allTransactions.addAll(sentTransactions);
         allTransactions.addAll(receivedTransactions);
         allTransactions.sort( Comparator.comparing(Transaction ::getTransactionCreatedAt).reversed());
-        return allTransactions;
+
+        return allTransactions.stream()
+                .map(TransactionHistory::new)
+                .toList();
     }
 
 }
