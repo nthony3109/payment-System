fd732e6aaf532094e6199db5e1ac0b42946d8db8
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-06-26 16:18:45 +0100

    updated

--- src/main/java/com/payement/wallet/Service/interfaces/TransactionService.java
+++ src/main/java/com/payement/wallet/Service/interfaces/TransactionService.java
@@ -1,9 +1,6 @@
 package com.payement.wallet.Service.interfaces;
 
-import com.payement.wallet.DTOs.DepositRes;
-import com.payement.wallet.DTOs.TransactionHistory;
-import com.payement.wallet.DTOs.TransferReq;
-import com.payement.wallet.DTOs.TransferRes;
+import com.payement.wallet.DTOs.*;
 import com.payement.wallet.Entity.Account;
 import com.payement.wallet.Entity.Transaction;
 
@@ -11,9 +8,10 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface TransactionService {
-    DepositRes deposit (BigDecimal amount, String accountNumber);
+    DepositRes deposit (DepositReq req);
     TransferRes transfer(TransferReq req);
     DepositRes logDepositTransaction(Account toaccount, BigDecimal amount);
     TransferRes logTransferTransaction(Account fromAccount, Account toAccount, BigDecimal amount, String description);
     List<TransactionHistory> getTransactionHistory(Account account);
+    List<TransactionHistory> getTransactionHistory(Account account, int page, int size);
 }
