3bc525f0789373bd53ce82ea28258b8f94c7d0ee
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-06-26 16:17:07 +0100

    added more methods

--- src/main/java/com/payement/wallet/Service/Implementations/UserServiceImple.java
+++ src/main/java/com/payement/wallet/Service/Implementations/UserServiceImple.java
@@ -1,27 +1,34 @@
-package com.payement.wallet.Service;
+package com.payement.wallet.Service.Implementations;
 
 import com.payement.wallet.DTOs.RegisterUserReq;
 import com.payement.wallet.Entity.UserEntity;
 import com.payement.wallet.Enum.Status;
 import com.payement.wallet.Repo.UserRepo;
+import com.payement.wallet.Service.interfaces.UserService;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.security.SecureRandom;
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+@Slf4j
+public class UserServiceImple implements UserService {
     private final UserRepo userRepo;
+    private  final SecureRandom secureRandom = new SecureRandom();
+private final RedisTemplate<String, String> redisTemplate;
 
+    @Override
     public boolean registerUser(RegisterUserReq req) {
         boolean userExistByEmail = userRepo.existsByEmail(req.getEmail());
         boolean userExistByPhoneNumber = userRepo.existsByPhoneNumber(req.getPhoneNumber());
         if (userExistByEmail || userExistByPhoneNumber) {
             return false;
         }
-        System.out.println("this is the username:" + req.getUsername());
+        log.info("this is the username: {}" ,req.getUsername());
         UserEntity user = UserEntity.builder()
                 .firstName(req.getFirstName())
                 .lastName(req.getLastName())
@@ -33,6 +40,22 @@ public class UserService {
                 .status(Status.UNVERIFIED)
                 .build();
         userRepo.save(user);
+        String otp = generateVerificationCode();
+        sendOtp(otp);
+        String userId = getUserId(req.getPhoneNumber()).toString();
+        String key = userId + " otp";
+        redisTemplate.opsForValue().set(key, otp, Duration.ofMinutes(15));
         return true;
+    }
+    public  String generateVerificationCode() {
+        log.info("building otp");
+        int  otp = 1000 + secureRandom.nextInt(9000);
+        return String.valueOf(otp);
+    }
+    public  void  sendOtp(String otp) {
+        log.info("the otp to be sent is: {}" , otp);
+    }
+    public  Long getUserId(String phoneNumber) {
+        return userRepo.findByPhoneNumber(phoneNumber).getId();
     }
 }
