131fd17960f57811f712dc01d2f71d367358034f
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-08-13 12:15:55 +0100

    otp verify dto

---
+++ src/main/java/com/payement/wallet/DTOs/VerifyOtpReq.java
@@ -0,0 +1,13 @@
+package com.payement.wallet.DTOs;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class VerifyOtpReq {
+    private  String userId;
+    private int  otp;
+}
