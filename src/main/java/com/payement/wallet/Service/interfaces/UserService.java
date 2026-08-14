2034ebbc4e8ed7499394afc63c8248c81c34d311
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-08-14 21:09:26 +0100

    updated

--- src/main/java/com/payement/wallet/Service/interfaces/UserService.java
+++ src/main/java/com/payement/wallet/Service/interfaces/UserService.java
@@ -6,4 +6,10 @@ import com.payement.wallet.Entity.UserEntity;
 public interface UserService {
     // more are coming for login and other user related functionalities
     boolean registerUser(RegisterUserReq req);
+    String verifyOtp(String userId, String otp);
+    String generateVerificationCode();
+    void  sendOtp(String mail,String otp);
+    Long getUserId(String phoneNumber);
+    void  setUserActive(Long id );
+
 }
