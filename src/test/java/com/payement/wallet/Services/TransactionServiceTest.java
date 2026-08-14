6422f334da4752fc66bc0a9beb985a7fc7294ab8
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-08-14 21:22:27 +0100

    update the service class

--- src/test/java/com/payement/wallet/Services/TransactionServiceTest.java
+++ src/test/java/com/payement/wallet/Services/TransactionServiceTest.java
@@ -1,21 +1,21 @@
 package com.payement.wallet.Services;
 
+import com.payement.wallet.DTOs.DepositReq;
+import com.payement.wallet.DTOs.DepositRes;
 import com.payement.wallet.Entity.Account;
 import com.payement.wallet.Entity.Transaction;
 import com.payement.wallet.Enum.Currency;
 import com.payement.wallet.Repo.AccountRepo;
 import com.payement.wallet.Repo.TransactionRepo;
-import com.payement.wallet.Service.TransactionService;
+import com.payement.wallet.Service.Implementations.TransactionServiceImple;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
@@ -25,7 +25,7 @@ import static org.mockito.Mockito.when;
 @Slf4j
 public class TransactionServiceTest {
     @InjectMocks
-    private  TransactionService tnxService;
+    private TransactionServiceImple tnxService;
     @Mock
     private AccountRepo acctRepo;
     @Mock
@@ -44,7 +44,7 @@ public class TransactionServiceTest {
         BigDecimal amount = new BigDecimal(1000);
         log.info("attempting to deposit {} into account number {}", amount, acctNum);
         when(acctRepo.findByAccountNumber(acctNum)).thenReturn(acct);
-        Transaction deposited = tnxService.deposit(amount, acctNum);
+        DepositRes deposited = tnxService.deposit(DepositReq.builder().accountNumber(acctNum).amount(amount).transactionRef("txn_ref_1").build());
         assert deposited != null;
         log.info("deposit successful, transaction reference: {}", deposited.getTransactionRef());
         verify(acctRepo).findByAccountNumber(acctNum);
@@ -65,7 +65,7 @@ public class TransactionServiceTest {
         BigDecimal amount = new BigDecimal(1000);
         when(tnxRepo.save(any(Transaction.class))).thenAnswer(invocation ->  invocation.getArgument(0, Transaction.class));
         log.info("caling method to create log");
-        Transaction tnx = tnxService.logDepositTransaction(acct, amount);
+        DepositRes tnx = tnxService.logDepositTransaction(acct, amount);
         log.info("transaction logged: {}", tnx);
         assert tnx != null;
         log.info("logged deposit transaction: {}", tnx.getTransactionRef());
