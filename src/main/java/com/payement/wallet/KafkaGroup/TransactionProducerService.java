fed9768829ece39b0357d3db4867fb656ac05c58
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-08-26 00:13:29 +0100

    event producer

---
+++ src/main/java/com/payement/wallet/KafkaGroup/TransactionProducerService.java
@@ -0,0 +1,19 @@
+package com.payement.wallet.KafkaGroup;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TransactionProducerService {
+    private static  final String TOPIC = "transaction-events";
+
+
+    public  void publishTransaction(TransactionKafkaPayLoad payLoad) {
+        String Key = payLoad.fromAccountNumber() != null ? payLoad.fromAccountNumber() : payLoad.toAccountNumber();
+
+        log.info("publishing to kafka ref: {}, type : {}",payLoad.transactionRef(),payLoad.type());
+    }
+}
