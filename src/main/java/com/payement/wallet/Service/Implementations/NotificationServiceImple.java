269cb73d05aaef643ef2bc19472a0c42c64ae618
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-08-31 21:34:53 +0100

    updated the service

--- src/main/java/com/payement/wallet/Service/Implementations/NotificationServiceImple.java
+++ src/main/java/com/payement/wallet/Service/Implementations/NotificationServiceImple.java
@@ -1,29 +1,34 @@
-package com.payement.wallet.Service;
+package com.payement.wallet.Service.Implementations;
 
 import com.payement.wallet.Entity.Account;
 import com.payement.wallet.Entity.Notification;
 import com.payement.wallet.Entity.UserEntity;
 import com.payement.wallet.Events.TransactionEvent;
 import com.payement.wallet.Exceptions.AccountNotFoundException;
+import com.payement.wallet.KafkaGroup.TransactionKafkaPayLoad;
 import com.payement.wallet.Repo.AccountRepo;
 import com.payement.wallet.Repo.NotificationRepo;
 import com.payement.wallet.Repo.UserRepo;
+import com.payement.wallet.Service.EmailService;
+import com.payement.wallet.Service.interfaces.NotificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class NotificationService {
+public class NotificationServiceImple implements NotificationService {
     private final NotificationRepo repo;
     private final UserRepo userRepo;
     private final AccountRepo accountRepo;
-    private final SimpMessagingTemplate template;
+    private final EmailService emailService;
 
-    // to user for notification operation
+    // to get user for notification operation
     private UserEntity getUser(String accountNumber){
         Account account = accountRepo.findByAccountNumber(accountNumber);
         UserEntity user = account.getUser();
@@ -33,25 +38,48 @@ public class NotificationService {
         return user;
     }
 
+    @Override
     public List<Notification> getAllNotification(String accountNumber) {
 
         return repo.findByUser(getUser(accountNumber));
     }
     //readOrUnread must be true for read messages and false for unread messages
+
+    @Override
     public List<Notification> FilterNotification(String AccountNumber, boolean readOrUnread) {
         return repo.findByUserAndIsViewed(getUser(AccountNumber),readOrUnread);
     }
+
     //continue with the count method to return read and unread messages number
+    @Override
     public long countReadAndUnreadmessages(UserEntity user, boolean readOrUnread) {
         return
                 repo.countByUserAndIsViewed(getUser("accountNumber"),
                         readOrUnread);
     }
 
-    @EventListener
-    public void handleTnxEvents(TransactionEvent event) {
-        UserEntity user = event.getToAccount().getUser();
+    //send notification section
+    @Override
+    public  void sendDepositNotification(TransactionKafkaPayLoad payLoad) {
+        log.info("handing over to email service to send deposit received mail");
+        emailService.sendDepositAlertByMail(payLoad);
 
     }
+    @Override
+    public void sendDebitNotification(TransactionKafkaPayLoad payLoad) {
+        log.info("handing over to email service to send debit mail");
+        emailService.sendDebitMail(payLoad);
+
+    }
+
+    @Override
+    public  void sendCreditNotification(TransactionKafkaPayLoad payLoad) {
+        log.info("handing over to email service to send credit mail");
+        emailService.sendCreditMail(payLoad);
+
+    }
+
+
+
 
 }
