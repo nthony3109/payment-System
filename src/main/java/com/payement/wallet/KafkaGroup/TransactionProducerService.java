7953823557c80bb44c585cd52b980d83ede2c66a
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-09-03 23:57:36 +0100

    git add TransactionProducerService.java

--- src/main/java/com/payement/wallet/KafkaGroup/TransactionProducerService.java
+++ src/main/java/com/payement/wallet/KafkaGroup/TransactionProducerService.java
@@ -2,6 +2,7 @@ package com.payement.wallet.KafkaGroup;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,11 +10,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TransactionProducerService {
     private static  final String TOPIC = "transaction-events";
+    private final KafkaTemplate<String, TransactionKafkaPayLoad> kafkaTemplate;
 
 
     public  void publishTransaction(TransactionKafkaPayLoad payLoad) {
         String Key = payLoad.fromAccountNumber() != null ? payLoad.fromAccountNumber() : payLoad.toAccountNumber();
 
         log.info("publishing to kafka ref: {}, type : {}",payLoad.transactionRef(),payLoad.type());
+        kafkaTemplate.send(TOPIC,Key,payLoad);
     }
 }
